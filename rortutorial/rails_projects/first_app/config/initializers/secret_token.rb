89323e68c43ac583077ce4fd516f28e7dbb5907e
dasc1 <david1.scott+github@gmail.com>
2013-04-06 21:00:35 +0100

    Added new book, rortutorial (Ruby on Rails Tutorial), with first_app.

---
+++ rortutorial/rails_projects/first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '53f6739cac55e04e96cf68e18b2fa8e38c7e53f9d1bb5b888eef81bbd20cdc3e55eb2123c77aa6a6e07aba516b7be33c58c80c9c0f95ab8e26d1a874815ae24c'
